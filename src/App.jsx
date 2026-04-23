46107d74558363b65cbf2a3d2252758f32becb48
manjarrezcristian <cristiantovarm@gmail.com>
2026-04-23 10:50:00 -0500

    Inicializa_Proyecto_React_Co_ Vite_y_Configuracion_Base

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import AppRouter from "./routes/AppRouter";
+
+// Mi aplicación principal de React
+function App() {
+  // Renderiza el enrutador de la app
+  return <AppRouter />;
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+// Configuración de Vite para el proyecto React
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+})
